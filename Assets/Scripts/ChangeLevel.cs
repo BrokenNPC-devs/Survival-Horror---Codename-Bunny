7402c6733434226f71b1a22e11d8c95909daaf2c
Ismail Siddiqui <xismailsiddiquix@gmail.com>
2016-04-22 16:52:38 -0500

    Initial Commit

---
+++ Assets/Scripts/ChangeLevel.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ChangeLevel : MonoBehaviour
+{
+    public string levelToLoad;
+    private bool playerInTrigger;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerInTrigger = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInTrigger && Input.GetAxis("Vertical") > 0)
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D sprite)
+    {
+        if (sprite.tag == "Player")
+        {
+            playerInTrigger = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D sprite)
+    {
+        if (sprite.tag == "Player")
+        {
+            playerInTrigger = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    float move = 0;
+    public float walkSpeed = 10;
+    bool facingLeft = true;
+    Rigidbody2D rb;
+
+    //flips the sprite depending on which way it is moving
+    void Flip() {
+        //character is no longer facing right
+        facingLeft = !facingLeft;  
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        //reverse axis
+        transform.localScale = theScale; 
+    }
+
+	// Use this for initialization
+	void Start() {
+        //for ease of writing
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update() {
+        move = Input.GetAxisRaw( "Horizontal");
+
+        if (move > 0 && facingLeft)
+            Flip();
+        if (move < 0 && !facingLeft)
+            Flip();
+	}
+
+    //use for RigidBody physics
+    void FixedUpdate() {
+        rb.velocity = new Vector2(move * walkSpeed, 0);
+    }
+}
